2f9401e8862976081f7171164a8fa7a56ce195e7
Ludovic <44300957+ludo67100@users.noreply.github.com>
2019-12-02 12:35:44 +0100

    new init for module loading

---
+++ electroPyy/core/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 14:54:51 2019
+
+@author: Ludovic.SPAETH
+"""
+
+from electroPyy.core.AnalogSignal import AnalogSignal
+from electroPyy.core.Convolution import Convolution
+from electroPyy.core.DetectPeaks import DetectPeaks
+from electroPyy.core.Filters import Filters
+    
