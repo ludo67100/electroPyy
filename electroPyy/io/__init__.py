9659bb710b32219b892f78e72b7caf97576a006a
Ludovic <44300957+ludo67100@users.noreply.github.com>
2019-12-02 12:34:03 +0100

    new init for module class

---
+++ electroPyy/io/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 14:54:51 2019
+
+@author: Ludovic.SPAETH
+"""
+
+from electroPyy.io.BaseRawIO import BaseRawIO
+from electroPyy.io.HdF5IO import HdF5IO
+from electroPyy.io.NeuroExIO import NeuroExIO
+from electroPyy.io.WinWcpRawIO import WinWcpRawIO
