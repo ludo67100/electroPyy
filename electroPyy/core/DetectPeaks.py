68881cc675b2dee82ed2b081bbee9f955a34b5af
Ludovic <44300957+ludo67100@users.noreply.github.com>
2019-12-04 15:09:22 +0100

    add fft

--- electroPyy/core/DetectPeaks.py
+++ electroPyy/core/DetectPeaks.py
@@ -1,197 +1,132 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 21 15:01:48 2019
+Created on Thu Nov 21 14:58:20 2019
 
-@author: Ludovic.SPAETH
+@author: Sam Garcia (adapted by ludovic.spaeth)
+Script for convolution in scalogram design 
 """
 
-class DetectPeaks() : 
+import numpy as np
+from scipy.signal import resample
+from scipy.fftpack import fft,ifft,fftshift,fftfreq
+
+
+class Convolution():
     
     def __init__(self):
-
-        __author__ = "Modified by Ludovic Spaeth, original by Marcos Duarte, https://github.com/demotu/BMC"
-        __version__ = "1.0.5"
-        __license__ = "MIT"
-
-
-    def indexes(x, mph=None, mpd=1, threshold=0, edge='rising',
-                     kpsh=False, valley=False, show=False, ax=None):
-        import numpy as np
-    
-        """Detect peaks in data based on their amplitude and other features.
-    
-        Parameters
-        ----------
-        x : 1D array_like
-            data.
-        mph : {None, number}, optional (default = None)
-            detect peaks that are greater than minimum peak height (if parameter
-            `valley` is False) or peaks that are smaller than maximum peak height
-             (if parameter `valley` is True).
-        mpd : positive integer, optional (default = 1)
-            detect peaks that are at least separated by minimum peak distance (in
-            number of data).
-        threshold : positive number, optional (default = 0)
-            detect peaks (valleys) that are greater (smaller) than `threshold`
-            in relation to their immediate neighbors.
-        edge : {None, 'rising', 'falling', 'both'}, optional (default = 'rising')
-            for a flat peak, keep only the rising edge ('rising'), only the
-            falling edge ('falling'), both edges ('both'), or don't detect a
-            flat peak (None).
-        kpsh : bool, optional (default = False)
-            keep peaks with same height even if they are closer than `mpd`.
-        valley : bool, optional (default = False)
-            if True (1), detect valleys (local minima) instead of peaks.
-        show : bool, optional (default = False)
-            if True (1), plot data in matplotlib figure.
-        ax : a matplotlib.axes.Axes instance, optional (default = None).
-    
-        Returns
-        -------
-        ind : 1D array_like
-            indeces of the peaks in `x`.
-    
-        Notes
-        -----
-        The detection of valleys instead of peaks is performed internally by simply
-        negating the data: `ind_valleys = detect_peaks(-x)`
+        print ('Convolution module loaded')
         
-        The function can handle NaN's 
-    
-        See this IPython Notebook [1]_.
-    
-        References
-        ----------
-        .. [1] http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/DetectPeaks.ipynb
-    
-        Examples
-        --------
-        >>> from detect_peaks import detect_peaks
-        >>> x = np.random.randn(100)
-        >>> x[60:81] = np.nan
-        >>> # detect all peaks and plot data
-        >>> ind = detect_peaks(x, show=True)
-        >>> print(ind)
-    
-        >>> x = np.sin(2*np.pi*5*np.linspace(0, 1, 200)) + np.random.randn(200)/5
-        >>> # set minimum peak height = 0 and minimum peak distance = 20
-        >>> detect_peaks(x, mph=0, mpd=20, show=True)
-    
-        >>> x = [0, 1, 0, 2, 0, 3, 0, 2, 0, 1, 0]
-        >>> # set minimum peak distance = 2
-        >>> detect_peaks(x, mpd=2, show=True)
-    
-        >>> x = np.sin(2*np.pi*5*np.linspace(0, 1, 200)) + np.random.randn(200)/5
-        >>> # detection of valleys instead of peaks
-        >>> detect_peaks(x, mph=-1.2, mpd=20, valley=True, show=True)
-    
-        >>> x = [0, 1, 1, 0, 1, 1, 0]
-        >>> # detect both edges
-        >>> detect_peaks(x, edge='both', show=True)
-    
-        >>> x = [-2, 1, -2, 2, 1, 1, 3, 0]
-        >>> # set threshold = 2
-        >>> detect_peaks(x, threshold = 2, show=True)
-    
-        Version history
-        ---------------
-        '1.0.5':
-            The sign of `mph` is inverted if parameter `valley` is True
+    def generate_wavelet_fourier(len_wavelet,
+                                 f_start,
+                                 f_stop,
+                                 deltafreq,
+                                 sampling_rate,
+                                 f0,
+                                 normalization):
         
-        """
-    
-        x = np.atleast_1d(x).astype('float64')
-        if x.size < 3:
-            return np.array([], dtype=int)
-        if valley:
-            x = -x
-            if mph is not None:
-                mph = -mph
-        # find indices of all peaks
-        dx = x[1:] - x[:-1]
-        # handle NaN's
-        indnan = np.where(np.isnan(x))[0]
-        if indnan.size:
-            x[indnan] = np.inf
-            dx[np.where(np.isnan(dx))[0]] = np.inf
-        ine, ire, ife = np.array([[], [], []], dtype=int)
-        if not edge:
-            ine = np.where((np.hstack((dx, 0)) < 0) & (np.hstack((0, dx)) > 0))[0]
-        else:
-            if edge.lower() in ['rising', 'both']:
-                ire = np.where((np.hstack((dx, 0)) <= 0) & (np.hstack((0, dx)) > 0))[0]
-            if edge.lower() in ['falling', 'both']:
-                ife = np.where((np.hstack((dx, 0)) < 0) & (np.hstack((0, dx)) >= 0))[0]
-        ind = np.unique(np.hstack((ine, ire, ife)))
-        # handle NaN's
-        if ind.size and indnan.size:
-            # NaN's and values close to NaN's cannot be peaks
-            ind = ind[np.in1d(ind, np.unique(np.hstack((indnan, indnan-1, indnan+1))), invert=True)]
-        # first and last values of x cannot be peaks
-        if ind.size and ind[0] == 0:
-            ind = ind[1:]
-        if ind.size and ind[-1] == x.size-1:
-            ind = ind[:-1]
-        # remove peaks < minimum peak height
-        if ind.size and mph is not None:
-            ind = ind[x[ind] >= mph]
-        # remove peaks - neighbors < threshold
-        if ind.size and threshold > 0:
-            dx = np.min(np.vstack([x[ind]-x[ind-1], x[ind]-x[ind+1]]), axis=0)
-            ind = np.delete(ind, np.where(dx < threshold)[0])
-        # detect small peaks closer than minimum peak distance
-        if ind.size and mpd > 1:
-            ind = ind[np.argsort(x[ind])][::-1]  # sort ind by peak height
-            idel = np.zeros(ind.size, dtype=bool)
-            for i in range(ind.size):
-                if not idel[i]:
-                    # keep peaks with the same height if kpsh is True
-                    idel = idel | (ind >= ind[i] - mpd) & (ind <= ind[i] + mpd) \
-                        & (x[ind[i]] > x[ind] if kpsh else True)
-                    idel[i] = 0  # Keep current peak
-            # remove the small peaks and sort back the indices by their occurrence
-            ind = np.sort(ind[~idel])
-    
-        if show:
-            if indnan.size:
-                x[indnan] = np.nan
-            if valley:
-                x = -x
-                if mph is not None:
-                    mph = -mph
-            DetectPeaks._plot(x, mph, mpd, threshold, edge, valley, ax, ind)
-    
-        return ind
-    
-    
-    def _plot(x, mph, mpd, threshold, edge, valley, ax, ind):
+        '''
+        Computes the wavelet coefficients at all scales and makes corresponding Fourier transform
+        When different signal scalograms are computed with the exact same coefficient
+        this function can be excecuted once and the result is passed directly to compute_morlet_scalogram
         
-        import numpy as np
+        Output = wf : Fourier transform of the waverlet coefficients (after weighting), Fourier freq at first
+        '''
         
-        """Plot results of the detect_peaks function, see its help."""
-        try:
-            import matplotlib.pyplot as plt
-        except ImportError:
-            print('matplotlib is not available.')
-        else:
-            if ax is None:
-                _, ax = plt.subplots(1, 1, figsize=(8, 4))
+        #Computes final map scales
+        scales = f0/np.arange(f_start,f_stop,deltafreq)*sampling_rate
+       
+        #compute wavelet coeffs at all scales
+        xi = np.arange(-len_wavelet/2.0,len_wavelet/2.0)
+        xsd = xi[:,np.newaxis()] / scales
+        wavelet_coefs = np.exp(complex(1j)*2.0*np.pi*f0*xsd)*np.exp(-np.power(xsd,2)/2.)
+        
+        weighting_function = lambda x: x**(-(1.0))
+        wavelet_coefs = wavelet_coefs*weighting_function(scales[np.newaxis(),:])
+        
+        #Transform the wavelet into the Fourier domain 
+        wf=fft(wavelet_coefs,axis=0)
+        wf=wf.conj() #Used to be an error here
+        
+        
+        return wf 
     
-            ax.plot(x, 'b', lw=1)
-            if ind.size:
-                label = 'valley' if valley else 'peak'
-                label = label + 's' if ind.size > 1 else label
-                ax.plot(ind, x[ind], '+', mfc=None, mec='r', mew=2, ms=8,
-                        label='%d %s' % (ind.size, label))
-                ax.legend(loc='best', framealpha=.5, numpoints=1)
-            ax.set_xlim(-.02*x.size, x.size*1.02-1)
-            ymin, ymax = x[np.isfinite(x)].min(), x[np.isfinite(x)].max()
-            yrange = ymax - ymin if ymax > ymin else 1
-            ax.set_ylim(ymin - 0.1*yrange, ymax + 0.1*yrange)
-            ax.set_xlabel('Data #', fontsize=14)
-            ax.set_ylabel('Amplitude', fontsize=14)
-            mode = 'Valley detection' if valley else 'Peak detection'
-            ax.set_title("%s (mph=%s, mpd=%d, threshold=%s, edge='%s')"
-                         % (mode, str(mph), mpd, str(threshold), edge))
-            # plt.grid()
-            plt.show()
+    
+    def compute_morlet_scalogram(ana,
+                                 f_start=5,
+                                 f_stop=100,
+                                 deltafreq=1,
+                                 sampling_rate=200,
+                                 t_start=-np.inf,
+                                 t_stop=np.inf,
+                                 f0=2.5,
+                                 normalisation=0,
+                                 wf=None):
+        
+        #Reduce the signal to the limits
+        sig = ana.signal[(ana.t()>=t_start)&(ana.t()<=t_stop)]
+        
+        if sig.size>0:
+            if wf is None:
+                if ana.sampling_rate != sampling_rate:
+                    sig = resample(sig,sig.size*sampling_rate/ana.sampling_rate)
+                wf = Convolution.generate_wavelet_fourier(sig.size,max(f_start,deltafreq),min(f_stop,ana.sampling_rate/2.),deltafreq,sampling_rate,f0,normalisation)
+            else:
+                if sig.size != wf.shape[0]:
+                    sig=resample(sig,wf.shape[0])
+                    
+            #Transform the signal in Fourier domain 
+            sigf=fft(sig)
+            
+            #Convolve (mult. in Fourier space)
+            wt_tmp = ifft(sigf[:,np.newaxis()]*wf,axis=0)
+            
+            #Shift output from ifft
+            wt = fftshift(wt_tmp,axes=[0])
+            
+        else: 
+            scales = f0/np.arange(f_start,f_stop,deltafreq)*sampling_rate
+            wt = np.empty((0,scales.size),dtype='complex')
+            
+        return wt 
+    
+    def FFT(signal,sampling_rate,plot=True,freq_range=[0,200]):
+        
+        '''
+        signal (array) : the signal
+        sampling_rate (int or float) : signal sampling rate in Hz
+        '''
+        
+        signal = np.asarray(signal)
+        
+        time_vector = np.arange(0,len(signal),1)/sampling_rate
+        
+        timewindow = signal.size
+        period = 1./sampling_rate
+        # Fourrier transform
+        signal_fft = abs(fft(signal))    # abs to get real part only
+        
+        # get frequential domain
+        signal_freq = fftfreq(timewindow,period)
+        
+        # Get real values from FFT and frequency domain
+        signal_fft = signal_fft[0:len(signal_fft)//2]
+        signal_freq = signal_freq[0:len(signal_freq)//2]
+        
+        if plot==True: 
+            from matplotlib import pyplot as plt
+        
+            fig, ax = plt.subplots(2,1)
+        
+            #Plot the signal
+            ax[0].set_title('signal')
+            ax[0].plot(time_vector, signal)
+            ax[0].set_xlabel('Time (s)'); ax[0].set_ylabel('Signal Amp')
+            
+            #Plot the spectrum
+            ax[1].plot(signal_freq,signal_fft)
+            ax[1].set_xlabel('Freq. range (Hz)'); ax[1].set_ylabel('Freq. Power')
+            ax[1].set_xlim(freq_range[0],freq_range[1])
+            plt.tight_layout()
+            plt.show(); 
+            
+        return signal_fft, signal_freq
